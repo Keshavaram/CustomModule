c95ce687115a42fa2f5ab95054e266b56488852b
Keshavaram <keshavjayanth1401@gmail.com>
2022-06-09 21:35:17 +0530

    Added the new module

---
+++ URLModule/src/main/java/blaash/gaming/mobile/urlmodule/ProcessURL.java
@@ -0,0 +1,14 @@
+package blaash.gaming.mobile.urlmodule;
+
+public class ProcessURL {
+    private final String URL;
+
+    public ProcessURL() {
+        this.URL = "https://www.codechef.com";
+    }
+
+    public String returnUrl()
+    {
+        return this.URL;
+    }
+}
---
+++ app/src/main/java/blaash/gaming/mobile/sdk/MainActivity.java
@@ -0,0 +1,14 @@
+package blaash.gaming.mobile.sdk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
